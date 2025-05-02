dd5eb99c07bede9997db6f32da4d5fbdbb274e98
SeunghwanWon <shwon0408@gmail.com>
2025-05-02 23:42:18 +0900

    feat: Day 10 - package and advanced import practice

---
+++ week02/my_package/__init__.py
@@ -0,0 +1,2 @@
+from .calculator import add, subtract, multiply, divide
+from .converter import km_to_miles, celsius_to_fahrenheit
---
+++ week02/my_package/converter.py
@@ -0,0 +1,11 @@
+def km_to_miles(km):
+    return km * 0.621371
+
+def celsius_to_fahrenheit(celsius):
+    return (celsius * 9/5) + 32
+
+def kg_to_lb(kg):
+    return kg * 2.20462
+
+def lb_to_kg(lb):
+    return lb / 2.20462
